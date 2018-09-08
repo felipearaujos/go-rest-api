0d1f374b353b9ee12731bb799fba550b327927ef
felipearaujos <felipe.araujo.s@live.com>
2018-09-07 21:24:40 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+var articles Articles
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	if articles != nil {
+		json.NewEncoder(w).Encode(articles)
+	}
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage")
+}
+
+func createArticle(w http.ResponseWriter, r *http.Request) {
+	var article Article
+	if r.Body == nil {
+		http.Error(w, "Please send a request body", 400)
+		return
+	}
+	err := json.NewDecoder(r.Body).Decode(&article)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	articles = append(articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func handlerRequest() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", createArticle).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
+func main() {
+	handlerRequest()
+}
