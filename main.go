d8058b3f8cd6b0d85a69138b526caaec3e774f75
felipearaujos <felipe.araujo.s@live.com>
2018-09-09 16:10:15 -0300

    get by id

--- main.go
+++ main.go
@@ -11,14 +11,29 @@ import (
 
 var articles Articles
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage")
+}
+
+func getArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	articleId, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Please insert the correct article Id", 400)
+		return
+	}
+
+	for i := range articles {
+		if articles[i].Id == articleId {
+			json.NewEncoder(w).Encode(articles[i])
+		}
+	}
+}
+
 func allArticles(w http.ResponseWriter, r *http.Request) {
 	if articles != nil {
 		json.NewEncoder(w).Encode(articles)
 	}
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage")
 }
 
 func deleteArticle(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +45,7 @@ func deleteArticle(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var tempArticles Articles
+
 	for i := range articles {
 		if articles[i].Id != articleId {
 			tempArticles = append(tempArticles, articles[i])
@@ -37,7 +53,6 @@ func deleteArticle(w http.ResponseWriter, r *http.Request) {
 	}
 
 	articles = tempArticles
-
 }
 
 func createArticle(w http.ResponseWriter, r *http.Request) {
